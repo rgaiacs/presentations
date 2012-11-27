4909e95e5aff8f61d5e110c6ab3d515e7f3fd60d
Raniere Silva <r.gaia.cs@gmail.com>
2012-11-27 17:21:35 -0200

    Some improves in bpsc

--- best_practices_for_scientific_computing/src/rect_area.f
+++ best_practices_for_scientific_computing/src/rect_area.f
@@ -1,5 +1,6 @@
       subroutine rect_area(x1, x2, y1, y2, a)
           ! This function attribute to `a` the area of the rectangle
           ! with opposite vertices in (x1, x2) and (y1, y2).
+          double precision x1, x2, y1, y2, a
           a = abs((x1 - y1) * (x2 - y2))
       end subroutine rect_area
